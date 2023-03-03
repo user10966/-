b647cc960568af21e261e4cfc1d009868694c34c
user10966 <126220655+user10966@users.noreply.github.com>
2023-03-03 11:13:20 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	srand(23415);
+	int x=rand(); 
+	printf("%d",x);
+	return 0;
+}
